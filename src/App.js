4c57b89ed9078d60f7bba3d1fb0cc03c96eb6936
Sebastian Ojefors <godnoken@gmail.com>
2021-08-06 16:52:57 +0200

    Rebuilding carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.scss";
 
+import React from "react";
+
 import codewars from "./images/codewars.png";
 import twitter from "./images/twitter.png";
 import codepen from "./images/codepen.png";
@@ -10,6 +12,8 @@ import pomodoro from "./images/pomodoro.png";
 import calculator from "./images/calculator.png";
 import quotes from "./images/quotes.png";
 import drum from "./images/drum.png";
+import downarrowround from "./images/downArrowRound.png";
+import uparrowround from "./images/upArrowRound.png";
 
 import emailjs from "emailjs-com";
 import { gsap } from "gsap";
@@ -80,19 +84,85 @@ function App() {
     }
   };
 
-  const handleProjectsSlider = () => {
-    let tween = gsap.timeline();
+  // Create array with all cards available
+  // Iterate array and use 3 elements and only 3 at all times
+  // Out of those 3, one is active and it is the middle one
+  // When clicking an arrow, which three items that are being iterated changes by 1 up or down
+  // All elements that are not being used can not be seen on screen
+
+  // Find active element
+
+  let tween = gsap.timeline()
+  const cardArray = [];
+
+  var width = 0;
+  const $ = (selector, context) => (context || document).querySelector(selector);
+
+  const container = $(".projects-container");
+
+  React.useEffect(() => {
+    width = $(".card").clientWidth;
+
+    window.addEventListener("resize", function(e) {
+      width = $(".card").clientWidth
+    });
+
+
+
+    //const cards = document.getElementsByClassName("card");
+    /*for (let i = 0; i < document.getElementsByClassName("card").length; i++) {
+      cardArray.push(cards[i]);
+    }*/
+    /*
+    cards[1].classList.add("activeCard");
+    tween.add("second");
+    tween.to(cards[0], {duration: 0, y: 0, x: 0, opacity: 0.05}, "second")
+    tween.to(cards[1], {duration:0, y: 0, x: 0, opacity: 1}, "second")
+    tween.to(cards[2], {duration: 0, y: 0, x: 0, opacity: 0.05}, "second")
+    */
+  }, [])
+  console.log(width);
+  
+  const handleProjectsSlider = (event) => {
+    console.log(width);
     
-    tween.from(".card", {
-      duration: 1,
-      rotation: 0
-    })
 
-    tween.to(".card", {
-      duration: 1,
-      rotation: 360
-    })
 
+    if (event.target.id === "upArrow" && gsap.globalTimeline.getChildren().filter(tween => tween.isActive()).length === 0) {
+    
+    
+    }
+
+    /*
+    if (event.target.id === "downArrow" && gsap.globalTimeline.getChildren().filter(tween => tween.isActive()).length === 0) {
+      
+    } else if (event.target.id === "upArrow" && gsap.globalTimeline.getChildren().filter(tween => tween.isActive()).length === 0) {
+      if (cards[0].classList[1] === "activeCard") {
+        tween.add("second")
+        tween.to(cards[0], {duration: 1, y: 0, x: 0, opacity: 0.05}, "second")
+        tween.to(cards[1], {duration: 1, y: 0, x: 0, opacity: 1}, "second")
+        tween.to(cards[2], {duration: 1, y: 0, x: 0, opacity: 0.05}, "second")
+        cards[1].classList.add("activeCard");
+        cards[0].classList.remove("activeCard");
+      } else if (cards[1].classList[1] === "activeCard") {
+        tween.add("first")
+        tween.to(cards[0], {duration: 1, y: "56.5vh", x: 0, opacity: 0.05}, "first")
+        tween.to(cards[1], {duration: 1, y: "-20vh", x: 0, opacity: 0.05}, "first")
+        tween.to(cards[2], {duration: 1, y: "-20vh", x: 0, opacity: 1}, "first")
+        cards[2].classList.add("activeCard");
+        cards[1].classList.remove("activeCard");
+      } else if (cards[2].classList[1] === "activeCard") {
+        tween.add("third")
+        tween.fromTo(cards[0], {duration: 1, y: "40vh", x: 0, opacity: 0.05}, {duration: 1, y: "20vh", x: 0, opacity: 1}, "third")
+        tween.to(cards[1], {duration: 1, y: "20vh", x: 0, opacity: 0.05}, "third")
+        tween.to(cards[2], {duration: 1, y: "-56.5vh", x: 0, opacity: 0.05}, "third")
+        cards[0].classList.add("activeCard");
+        cards[2].classList.remove("activeCard");
+      } else if (cards[3].classList[1] === "activeCard") {
+
+      }
+    }
+    */
   }
 
   return (
@@ -177,32 +247,30 @@ function App() {
           <h2>Minimalistic design.</h2>
         </div>
       </section>
-      {/*
-        Need to track each card somehow
-        Hide all inactive cards
-        Animate the container
-      */}
       <section id="projects">
-        <div className="arrow"></div>
-        <div id="card-container">
-          <div className="card" id="pomodoro" onClick={handleProjectsSlider}>
+        <div id="projects-container">
+          <img onClick={handleProjectsSlider} className="arrow" id="upArrow" src={uparrowround} alt="Arrow button moving projects up"/>
+          <div className="card" id="pomodoro">
             <img src={pomodoro} alt="pomodoro app" />
+            
             <p></p>
           </div>
           <div className="card" id="drum-machine">
             <img src={drum} alt="drum machine app" />
+            
             <p></p>
           </div>
           <div className="card" id="calculator">
             <img src={calculator} alt="calculator app" />
+            
             <p></p>
           </div>
           <div className="card" id="quotes">
             <img src={quotes} alt="quotes app" />
             <p></p>
           </div>
+          <img onClick={handleProjectsSlider} className="arrow" id="downArrow" src={downarrowround} alt="Arrow button moving projects down"/>
         </div>
-        <div className="arrow"></div>
       </section>
       <section id="skills">
         <div id="skills-header-container">
