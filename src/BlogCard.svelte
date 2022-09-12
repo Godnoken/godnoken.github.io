dce710f767bba9c478c8be30598d8f0d000c68a7
Sebastian Ojefors <godnoken@gmail.com>
2022-09-12 22:20:20 +0200

    Update all project texts

--- src/BlogCard.svelte
+++ src/BlogCard.svelte
@@ -4,7 +4,7 @@
 
     let image = "./images/blogHigh.png";
     let title: string = "Retro Blog";
-    let description: string = "<span style='display: flex; justify-content: center'>Second Angular project</span> <br> Hosted on <a href='https://blog.heroku.com/app_sleeping_on_heroku' target='_blank'>Heroku</a>. Please let the website boot up for 15~ seconds. <br><br> <em>Better</em> structure than the first one. I emphasize better because I honestly found it very hard to find best practice examples on Angular. It may be due to it being a framework for (mostly) bigger companies. <br><br> I designed this with a retro look in mind. It's looks alright but I felt pretty limited by css. <br><br> This is a very bare bones type of blog. Noone can create new blogs, but you can comment & quote other quest's comments and it is served with a neat pagination system. <br><br> I think I spent the better part of this project trying to understand caching. Bad move.";
+    let description: string = "<span style='display: flex; justify-content: center'>Second Angular project</span> <br><br> I designed this with a retro look in mind. It looks okay but I felt pretty limited by CSS. <br><br> It does have better structure than the first one, although I do want to emphasize better because I honestly found it very hard to find best practice examples on Angular. It may be due to it being a framework for (mostly) bigger companies. <br><br> It is very much a bare-bones blog. You can't create blog posts, but you can comment & quote other guests comments and it is served with a neat pagination system.";
     let codeUrl: string = "https://github.com/Godnoken/developer-challenge-api";
     let liveUrl: string = "https://mini-blog-letsgetchecked.herokuapp.com/blog";
     let elementIndex = 5;
--- src/WeatherCard.svelte
+++ src/WeatherCard.svelte
@@ -6,7 +6,7 @@ import type { windowWidth } from "./stores";
 
     let image = "./images/weatherHigh.png";
     let title: string = "Visual Weather";
-    let description: string = "<span style='display: flex; justify-content: center'>Clouds. Rain. Snow. Sun & Moon.</span><br><br> A weather app that is far from the generic weather app you've seen before. <br><br> It started with the idea of using one of google's map APIs and visualising the weather there. I figured it would perhaps be a bit too ambitious to start with, so I started visualising the current weather with a plain white background. <br><br> One visualisation lead to another, and here we are, only limited by the data I receive from <a href='https://openweathermap.org/api'>OpenWeatherMap</a> and performance. <br> This leads me to my next goal. Remake everything with canvas.";
+    let description: string = "<span style='display: flex; justify-content: center'>Clouds. Rain. Snow. Sun & Moon</span><br><br> A weather app that is far from the generic weather app you've seen before. <br><br> It started with the idea of using one of google's map APIs and visualising the weather there. I figured it would perhaps be a bit too ambitious to start with, so I started visualising the current weather with a plain white background. <br><br> One visualisation lead to another, and here we are, only limited by the data I receive from <a href='https://openweathermap.org/api'>OpenWeatherMap</a> and performance. <br> This leads me to my next goal. Remake everything with canvas.";
     let codeUrl: string = "https://github.com/Godnoken/weather";
     let liveUrl: string = "https://godnoken.github.io/weather/";
     let elementIndex = 2;
